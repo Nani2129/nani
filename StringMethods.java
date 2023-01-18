f6b2c81f3dfa34795eac9ef5e210c0bad36fff4f
Nani2129 <122969759+Nani2129@users.noreply.github.com>
2023-01-18 16:00:23 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,33 @@
+package String;
+
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		
+	String s="Welcome";
+		
+	//String contains
+	
+		//System.out.println(s.contains("elc"));
+		
+	//equals
+	
+		// System.out.println(s.equals("Welcome"));
+	
+	//equalsIgnore
+	
+	//System.out.println(s.equalsIgnoreCase("WeLcoME"));
+	
+	//Replace
+	
+	//System.out.println(s.replace("Welcome", "chandu"));
+	
+	//Substring
+	
+	System.out.println(s.substring(0,3));
+		
+		
+	}
+
+}
